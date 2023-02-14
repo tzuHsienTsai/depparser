ced70e4b51d69a4f78137df0cba934e98f90a098
tzuHsienTsai <tzu-hsien.tsai@deephow.com>
2023-02-14 22:15:25 +0800

    refine the process

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import stanza
 import streamlit as st
 
 puncList = ['.', ',', ':', ';', '!', '?', '\'', '\"', '，', '。', '：', '；', '！', '？']
+languageWithBlankSpace = ['en']
 
 def canMerge(bound:int, rootIndex:int, direction:int, dependentTree:list, theNumberOfChildrenInDependentTree:list, theNumberOfWords:int)->int:
 	if dependentTree[rootIndex][bound] == 1 and theNumberOfChildrenInDependentTree[bound] == 0:
@@ -42,106 +43,127 @@ def findConstituent(currentRootIndex: int, dependentTree: list, theNumberOfChild
 	return ret
 
 def dependencyParsing(language:str, sentence:str, lineLimit:int):
+	lineLimit -= 1
+	st.write('The length of the input sentence is:', len(sentence))
 	nlp = stanza.Pipeline(lang = language, processors = 'tokenize, pos, lemma, depparse')
 	doc = nlp(sentence)
+	print(*[f'word: {word.text}\tupos: {word.upos}\txpos: {word.xpos}\tfeats: {word.feats if word.feats else "_"}' for sent in doc.sentences for word in sent.words], sep='\n')
+	print(*[f'id: {word.id}\tword: {word.text}\thead id: {word.head}\thead: {sent.words[word.head-1].text if word.head > 0 else "root"}\tdeprel: {word.deprel}' for sent in doc.sentences for word in sent.words], sep='\n')
 
 	POSTagger = stanza.Pipeline(lang=language, processors='tokenize,pos')
 	POSdoc = nlp(sentence)
-	print(*[f'word: {word.text}\tupos: {word.upos}\txpos: {word.xpos}\tfeats: {word.feats if word.feats else "_"}' for sent in doc.sentences for word in sent.words], sep='\n')
-	POSOfWords = POSdoc.sentences[0].words
-
-	print(*[f'id: {word.id}\tword: {word.text}\thead id: {word.head}\thead: {sent.words[word.head-1].text if word.head > 0 else "root"}\tdeprel: {word.deprel}' for sent in doc.sentences for word in sent.words], sep='\n')
-
-	words = doc.sentences[0].words
-	theNumberOfWords = len(words)
-	#word.id, word.text, word.head
-	isWordsDependent = [[0]*(theNumberOfWords + 1) for _ in range(theNumberOfWords + 1)]
-	theNumberOfDependent = [0 for _ in range(theNumberOfWords + 1)]
-	for word in words:
-		isWordsDependent[word.head][word.id] = 1
-		theNumberOfDependent[word.head] += 1
-#	print(isWordsDependent)
-#	print(theNumberOfDependent)
-
-	currentRootIndex = 0
-	for index in range(theNumberOfWords + 1):
-		if isWordsDependent[currentRootIndex][index] == 1:
-			currentRootIndex = index
-			break
-
-	availableBreakpoint = findConstituent(currentRootIndex, isWordsDependent, theNumberOfDependent, theNumberOfWords)
-	
-	availableBreakpoint = sorted(availableBreakpoint)
-
-	idx = 0
-	while idx < len(availableBreakpoint):
-		if idx == len(availableBreakpoint) - 1:
-			break
-		currentInterval = availableBreakpoint[idx]
-		nextInterval = availableBreakpoint[idx + 1]
-		if (currentInterval[0] != currentInterval[1]) or POSOfWords[idx].upos == 'VERB':
-			idx += 1
-			continue
-		if isWordsDependent[currentInterval[1]][nextInterval[0]] == 1 or isWordsDependent[nextInterval[0]][currentInterval[1]] == 1:
-			availableBreakpoint.remove(currentInterval)
-			availableBreakpoint.remove(nextInterval)
-			availableBreakpoint.insert(idx, (currentInterval[0], nextInterval[1]))
-		else:
-			idx += 1
-			
-	print(availableBreakpoint)
-	st.write('The length of the input sentence is:', len(sentence))
 	lines = []
-	groupIndex = 0
-	currentLine = ''
-	while groupIndex < len(availableBreakpoint):
-		if doesntExceedLength(currentLine, words, availableBreakpoint[groupIndex], lineLimit):
-			for idx in range(availableBreakpoint[groupIndex][0] - 1, availableBreakpoint[groupIndex][1]):
-				if currentLine != '':
-					currentLine += ' '
-				currentLine += words[idx].text
-			groupIndex += 1
-		else:
-			for punc in puncList:
-				currentLine = currentLine.replace(' ' + punc, punc)
-			lines.append(currentLine.strip())
-			currentLine = ''
-	if currentLine != '':
-		for punc in puncList:
-			currentLine = currentLine.replace(' ' + punc, punc)
-		lines.append(currentLine.strip())
-	
-	for idx in range(1, len(lines)):
-		if lines[idx][0] in puncList:
-			lines[idx - 1] += lines[idx][0]
-			lines[idx] = lines[idx][1:]
-			while lines[idx][0] == ' ':
-				if len(lines[idx]) == 1:
-					lines.pop()
-					break
-				else:
-					lines[idx] = lines[idx][1:]
+#	print(len(doc.sentences))
+#	print(len(POSdoc.sentences))
+	for sentenceIdx in range(len(doc.sentences)):
+		POSOfWords = POSdoc.sentences[sentenceIdx].words
 
-	st.write('The input sentence is split into ' + str(len(lines)) + ' lines.')
+		words = doc.sentences[sentenceIdx].words
+		theNumberOfWords = len(words)
+		isWordsDependent = [[0]*(theNumberOfWords + 1) for _ in range(theNumberOfWords + 1)]
+		theNumberOfDependent = [0 for _ in range(theNumberOfWords + 1)]
+		for word in words:
+			isWordsDependent[word.head][word.id] = 1
+			theNumberOfDependent[word.head] += 1
+
+		currentRootIndex = 0
+		for index in range(theNumberOfWords + 1):
+			if isWordsDependent[currentRootIndex][index] == 1:
+				currentRootIndex = index
+				break
+
+		constituents = findConstituent(currentRootIndex, isWordsDependent, theNumberOfDependent, theNumberOfWords)
+	
+		constituents = sorted(constituents)
+
+		idx = 0
+		while idx < len(constituents):
+			if idx == len(constituents) - 1:
+				break
+			currentInterval = constituents[idx]
+			nextInterval = constituents[idx + 1]
+			if (currentInterval[0] != currentInterval[1]) or POSOfWords[idx].upos == 'VERB':
+				idx += 1
+				continue
+			if isWordsDependent[currentInterval[1]][nextInterval[0]] == 1 or isWordsDependent[nextInterval[0]][currentInterval[1]] == 1:
+				constituents.remove(currentInterval)
+				constituents.remove(nextInterval)
+				constituents.insert(idx, (currentInterval[0], nextInterval[1]))
+			else:
+				idx += 1
+				
+		print(constituents)
+
+		groupIndex = 0
+		currentLine = ''
+		while groupIndex < len(constituents):
+			remainLength = countConstituentsLength(groupIndex, len(constituents), constituents, words, language)
+			if currentLine == '' and remainLength - 1 <= lineLimit * 2:
+				splitPoint = groupIndex
+				bestLength = remainLength
+				for candidateSplitPoint in range(groupIndex, len(constituents)):
+					currentLength = countConstituentsLength(groupIndex, candidateSplitPoint, constituents, words, language)
+					if abs(currentLength * 2 - remainLength) < abs(bestLength * 2 - remainLength):
+						splitPoint = candidateSplitPoint
+						bestLength = currentLength
+
+				lines.append(makeSentence(groupIndex, splitPoint, constituents, words, language))
+				lines.append(makeSentence(splitPoint, len(constituents), constituents, words, language))
+				break
+			else:
+				tailIndex = groupIndex + 1
+				while countConstituentsLength(groupIndex, tailIndex, constituents, words, language) <= lineLimit:
+					tailIndex += 1
+				lines.append(makeSentence(groupIndex, tailIndex, constituents, words, language))
+				groupIndex = tailIndex
+
+		for idx in range(1, len(lines)):
+			if lines[idx][0] in puncList:
+				lines[idx - 1] += lines[idx][0]
+				lines[idx] = lines[idx][1:]
+				lines[idx] = lines[idx].strip()
+				if len(lines[idx]) == 0:
+					lines.remove(lines[idx])
+					idx -= 1
+				
 	st.write(lines)
-	st.write('Each of them is of length:')
-	lineLength = [len(line) for line in lines]
-	st.write(lineLength)
+	for lineIdx in range(len(lines)):
+		st.write('Line ' + str(lineIdx) + ': ' + lines[lineIdx])
+		st.write('with length ' + str(len(lines[lineIdx])))
 	return
 
-def doesntExceedLength(line, words, wordRange, lineLimit):
-	remainLength = lineLimit - len(line)
-	for idx in range(wordRange[0] - 1, wordRange[1]):
-		remainLength -= len(words[idx].text)
-		if remainLength < lineLimit and words[idx].text not in puncList:
-			remainLength -= 1
-		if remainLength < 0:
-			return False
-	return True
+def countConstituentsLength(headIndex, tailIndex, constituents, words, language):
+	totalLength = 0
+	for constituentIndex in range(headIndex, tailIndex):
+		for idx in range(constituents[constituentIndex][0] - 1, constituents[constituentIndex][1]):
+			totalLength += len(words[idx].text)
+	if language in languageWithBlankSpace:
+		totalLength += (len(constituents) - 1) - headIndex - 1 - 1
+	return totalLength
+
+def makeSentence(headIndex, tailIndex, constituents, words, language):
+	sentence = ''
+	for constituentIdx in range(headIndex, tailIndex):
+		for wordIdx in range(constituents[constituentIdx][0] - 1, constituents[constituentIdx][1]):
+			if (constituentIdx != headIndex or wordIdx != constituents[constituentIdx][0] - 1) and language in languageWithBlankSpace:
+				sentence += ' '
+			sentence += words[wordIdx].text
+	sentence = sentence.strip()
+
+	for punc in puncList:
+		sentence = sentence.replace(' ' + punc, punc)
+	sentence = sentence.replace(' - ', '-')
+	sentence = sentence.replace(' n\'t', 'n\'t')
+	sentence = sentence.replace(' \'s', '\'s')
+	sentence = sentence.replace(' \'d', '\'d')
+	
+	return sentence
 
 if __name__ == '__main__':
 	st.title('Long Sentence Splitter')
 	language = st.selectbox('Please select the language of input.', ['en', 'zh', 'ja'])
 	sentence = st.text_input("Input Sentence:")
 	lineLimit = int(st.text_input("Line limit:"))
-	dependencyParsing(language, sentence, lineLimit)
+	starts = st.button('run')
+	if starts:
+		dependencyParsing(language, sentence, lineLimit)
